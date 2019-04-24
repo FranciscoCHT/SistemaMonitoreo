5034280cbd26274c1c2d7fd6d60a8ffee20ef75f
Francisco Chacón <francisco.chacont@gmail.com>
2019-04-24 01:32:05 -0400

    3.4 Configurada script nodoactual igual a nodo historico. Falta agregar gráficos gauge

--- SistemaMCE.WEB/Scripts/app/Nodos/nodoactual.ts
+++ SistemaMCE.WEB/Scripts/app/Nodos/nodoactual.ts
@@ -7,5 +7,222 @@ namespace Nodos {
     export class NodoActualIndexViewModel {
 
         //UNA MANERA DE HACERLO ES SACAR EL DATENOW POR JAVASCRIPT Y LUEGO SACAR EL GETDAY GETMONTH GETYEAR, PARA ASI DATASOURCE FILTER POR DAY = GETDAY MONTH=GETMONTH... ETC, SI NO, LLAMADA AJAX A UN METODO API CON PARAMETROS DIA MES Y AÑO
+
+        public lectura: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public nodos: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public idNodo: KnockoutObservable<any> = ko.observable<any>(-1);
+        public idSector: KnockoutObservable<any> = ko.observable<any>(-1);
+        public tipoNodos: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public nombreTipo: KnockoutObservable<any> = ko.observable<any>();
+        public sectores: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public usuarios: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public enable: KnockoutObservable<boolean> = ko.observable(true);
+        public idRow: KnockoutObservable<number> = ko.observable(0);
+        public idRowIndex: KnockoutObservable<number> = ko.observable(-1);
+
+        getLastWeek() {
+            var today = new Date();
+            today.setDate(today.getDate() - 22);
+            var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+            return lastWeek;
+        }
+
+        getNodosByUser(user: any, sect: any): void {
+            this.nodos([]);
+            let url = window.location.origin + '/api/nodos/' + sect + '/' + user;
+            $.ajax({
+                type: 'GET',
+                url: url,
+            }).done((data: any) => {
+                for (var i: number = 0; i < data.length; i++) {
+                    this.nodos.push({
+                        ID: data[i].ID,
+                        Nombre: data[i].Nombre,
+                        Tipo: data[i].Tipo,
+                        TipoStr: data[i].TipoStr,
+                        Estado: data[i].Estado,
+                        Voltaje: data[i].Voltaje,
+                        Sector: data[i].Sector,
+                        Usuario: data[i].Usuario
+                    });
+                }
+                this.idNodo(data[0].ID);
+                let chartHist = $('#chartHist').dxChart('instance');            //
+                let chartDataSource = chartHist.option('dataSource');           //  Esto recarga el datasource, lo que hace que se consulte de nuevo la lectura, con el nuevo IDNODO
+                chartDataSource.load();                                         //
+
+                let nodoFilterIns = $('#selectNodo').dxSelectBox('instance');       //
+                nodoFilterIns.option('value', this.idNodo());                       //  Esto pone el valor del filtro en el IDNODO actual
+                //let chartHist = $('#chartHist').dxChart('instance'); 
+                //let ds = chartHist.option('dataSource')
+                //chartHist.option('dataSource', ds);
+            }).fail((data: any) => {
+                DevExpress.ui.notify(data.responseJSON, "error", 3000);
+            });
+        }
+
+        getSectores(): void {
+            this.sectores([]);
+            let url = window.location.origin + '/api/sectores';
+            $.ajax({
+                type: 'GET',
+                url: url,
+            }).done((data: any) => {
+                this.sectores.push({
+                    ID: -1,
+                    Nombre: 'Todos'
+                });
+                for (var i: number = 0; i < data.length; i++) {
+                    this.sectores.push({
+                        ID: data[i].ID,
+                        Nombre: data[i].Nombre
+                    });
+                }
+            }).fail((data: any) => {
+                DevExpress.ui.notify(data.responseJSON, "error", 3000);
+            });
+        }
+
+        constructor() {
+            this.getNodosByUser(window.localStorage.getItem('user'), -1);
+            this.getSectores();
+        }
+
+        public customStore = new DevExpress.data.CustomStore({
+            load: (loadOptions: any) => {
+                var defer = $.Deferred();
+                $.getJSON(window.location.origin + '/api/nodos/lectura/' + this.idNodo()).done((data) => {
+                    for (var i: number = 0; i < data.length; i++) {
+                        data[i].FechaHoraJS = new Date(data[i].FechaHora);
+                        //data[i].FechaHoraUTC = Date.parse(data[i].FechaHoraUTC);                  // Esto para filtrar con date en milisegundos. Es con UTC ya que el date parse solo se puede con UTC String
+                        //let dateString = new Date(data[i].FechaHora).toString().split(" GMT");
+                        //data[i].FechaHoraString = dateString[0].slice(0, -3);
+                    }
+                    // Perform filtering on client side
+                    var query = DevExpress.data.query(data);
+                    if (loadOptions.filter) {
+                        query = query.filter(loadOptions.filter);
+                    }
+                    defer.resolve(query.toArray());
+                });
+                return defer.promise();
+            }
+        })
+
+        nodoFilter: any = {
+            dataSource: this.nodos,
+            width: "auto",
+            placeholder: "Seleccione nodo a visualizar...",
+            displayExpr: 'Nombre',
+            valueExpr: 'ID',
+            value: this.idNodo(),
+            onItemClick: (e) => {
+                this.idNodo(e.itemData.ID);
+                let chartHist = $('#chartHist').dxChart('instance');
+                let chartDataSource = chartHist.option('dataSource');
+                //let mas = this.mes(); //Hacer un SELECTBOX CON YEAR, luego al click, guardar el year en un observable, y despues un selectbox con seleccionar Mes, al hacer click, pasar el valor al filter y el year observable.
+                //let year = this.year();
+                //chartHist.option('dataSource', new DevExpress.data.DataSource({ //ME SERVIRÁ PARA PODER HACER UN FILTRO SELECCIONANDO UN MES Y CAMBIAR EL MES DE LOS GRAFICOS filter: [['Mes', '=', '3']]
+                //    store: this.customStore,
+                //    filter: [['Mes', '=', mas], "and", ['Año', '=', year]]
+                //}));
+                chartDataSource.load();
+            }
+        }
+
+        sectFilter: any = {
+            dataSource: this.sectores,
+            width: "auto",
+            placeholder: "Seleccione sector de nodos...",
+            displayExpr: 'Nombre',
+            valueExpr: 'ID',
+            value: this.idSector(),
+            onItemClick: (e) => {
+                this.idSector(e.itemData.ID);
+                this.getNodosByUser(window.localStorage.getItem('user'), this.idSector());
+                //let chartHist = $('#chartHist').dxChart('instance');
+                //let chartDataSource = chartHist.option('dataSource');
+                //chartDataSource.load();
+            }
+        }
+
+        chartHist: any = {
+            palette: "Dark Violet",
+            dataSource: new DevExpress.data.DataSource({
+                store: this.customStore,                                // v Esto para filtrar con date en milisegundos. v
+                filter: [['FechaHoraJS', '>=', this.getLastWeek()]]     //['FechaHoraUTC', '<=', Date.parse(new Date().toUTCString())], "and", ['FechaHoraUTC', '>=', Date.parse(new Date().toUTCString()) - 86400000*21]
+            }),
+            commonSeriesSettings: {
+                argumentField: "FechaHora"
+            },
+            margin: {
+                bottom: 20
+            },
+            argumentAxis: {
+                //valueMarginsEnabled: false,
+                discreteAxisDivisionMode: "crossLabels",
+                argumentType: "datetime",
+                aggregationInterval: "day",
+                grid: {
+                    visible: true
+                },
+                label: {
+                    overlappingBehavior: "rotate",
+                    rotationAngle: "-60",
+                    format: (FechaHora) => {
+                        var weekday = new Array(7);
+                        weekday[0] = "Domingo";
+                        weekday[1] = "Lunes";
+                        weekday[2] = "Martes";
+                        weekday[3] = "Miercoles";
+                        weekday[4] = "Jueves";
+                        weekday[5] = "Viernes";
+                        weekday[6] = "Sabado";
+                        return weekday[FechaHora.getDay()];
+                    }
+                },
+                tickInterval: { days: 1 }
+            },
+            series: [
+                {
+                    axis: "Kwh",
+                    color: "#e91e63",
+                    valueField: "Kwh",
+                    point: {
+                        size: 7
+                    },
+                    type: "bar",
+                    aggregation: {
+                        enabled: true,
+                        method: "sum"
+                    },
+                    name: "Kilowatt por Hora"
+                }
+            ],
+            legend: {
+                verticalAlignment: "bottom",
+                horizontalAlignment: "center",
+                itemTextPosition: "bottom"
+            },
+            title: {
+                text: "Consumo eléctrico esta semana",
+                subtitle: {
+                    text: "(Kilowatt por hora, día equivalente)"
+                }
+            },
+            "export": {
+                enabled: true
+            },
+            tooltip: {
+                enabled: true,
+                customizeTooltip: function (arg) {
+                    var options = { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric" };
+                    let slice = arg.argument.toLocaleString("es-ES", options).slice(0, -2);
+                    return {
+                        text: "Fecha: " + slice + "<br/>" + "KwH: " + arg.value
+                    }
+                }
+            }
+        }
     }
 }
--- SistemaMCE.WEB/Views/Nodos/ConsumoActual.vbhtml
+++ SistemaMCE.WEB/Views/Nodos/ConsumoActual.vbhtml
@@ -2,7 +2,7 @@
     ViewData("Title") = "ConsumoActual"
 End Code
 
-<section class="content-header">
+<section class="content-header" style="border-bottom: 1px solid #000;padding-bottom: 10px; margin-left:10px;margin-right: 10px;">
     <h1>
         <span style="text-shadow: 3px 3px 11px #a7a7a7; letter-spacing:1px;">Consumo actual</span>
         <small>Consumo actual de cada nodo del usuario a cargo.</small>
@@ -16,7 +16,30 @@ End Code
 
 <link rel="dx-theme" data-theme="generic.greenmist" href="https://cdn3.devexpress.com/jslib/18.2.5/css/dx.greenmist.css" />
 
-<div id="cuerpo" style="margin-bottom:70px" class="dx-fieldset">
+<div id="cuerpo" style="margin-bottom:70px; margin-top:20px;" class="dx-fieldset">
+    <span class="dx-form-group-caption" style="color: #6a828e;">Seleccione sector/nodo:</span>
+    <div class="dx-field" style="display: flex; flex-direction: row; justify-content: flex-start; align-items: stretch; width: auto; height: auto; flex-basis: 0px; flex-grow: 1; border-top: 1px solid #7eb2be; border-bottom: 1px solid #7eb2be; padding-bottom: 16px;padding-top: 8px; margin-top: 6px;margin-bottom: 24px;">
+        <div class="dx-item dx-box-item dx-item-content dx-box-item-content" style="display: flex; min-width: auto; flex: 1 1 0px; width: auto;height: auto;flex-basis: 0px;flex-grow: 1;flex-direction: column; padding-right: 15px;">
+            <label class="dx-field-item-label dx-field-item-label-location-top" for="dx_dx-e344b929-29e2-9de5-484a-2570a7dd3fa5_Sector">
+                <span class="dx-field-item-label-content" style="width: 46px;">
+                    <span class="dx-field-item-label-text">Sector:</span>
+                </span>
+            </label>
+            <div id="selectSect" data-bind="dxSelectBox: sectFilter"></div>
+        </div>
+        <div class="dx-item dx-box-item dx-item-content dx-box-item-content" style="display: flex; min-width: auto; flex: 1 1 0px; width: auto;height: auto;flex-basis: 0px;flex-grow: 1;flex-direction: column; padding-left: 15px;">
+            <label class="dx-field-item-label dx-field-item-label-location-top" for="dx_dx-e344b929-29e2-9de5-484a-2570a7dd3fa5_Nodo">
+                <span class="dx-field-item-label-content" style="width: 41px;">
+                    <span class="dx-field-item-label-text">Nodo:</span>
+                </span>
+            </label>
+            <div id="selectNodo" data-bind="dxSelectBox: nodoFilter"></div>
+        </div>
+    </div>
+
+    <div class="dx-field">
+        <div id="chartHist" data-bind="dxChart: chartHist"></div>
+    </div>
 
 </div>
 
